debd8599f468c9c989a227d4cfbacc7664d015b6
Jamie <gatorjuice@gmail.com>
2016-04-12 20:05:31 -0500

    added quotes

--- app/controllers/gurls_controller.rb
+++ app/controllers/gurls_controller.rb
@@ -10,7 +10,13 @@ class GurlsController < ApplicationController
         dest: destination,
         gatorly: (0...7).map { ('a'..'z').to_a[rand(26)] }.join
       )
-      if @gurl.save 
+      if @gurl.save
+        if params[:author] && params[:body]
+          Quote.create(
+            author: params[:author],
+            body: params[:body]
+          )
+        end
         redirect_to "/generate/#{@gurl.gatorly}"
       else
         render "new.html.erb"
@@ -22,6 +28,8 @@ class GurlsController < ApplicationController
 
   def show
     @gurl = Gurl.find_by(gatorly: params[:gatorly])
+    offset = rand(Quote.count)
+    @quote = Quote.offset(offset).first
   end
 
   def redirect
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412072937) do
+ActiveRecord::Schema.define(version: 20160413003001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160412072937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "gatorly"
+  end
+
+  create_table "quotes", force: :cascade do |t|
+    t.text     "body"
+    t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
